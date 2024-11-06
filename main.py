fc076f749aff0b81937bee81a9e72c0819261ce4
IOSHIHIRO <emirans23@gmail.com>
2024-11-06 14:24:18 +0600

    location

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import logging
 from bot_cod import bot, dp
 from handlers.start import start_router
 from handlers.menu import menu_router
+from handlers.geolocation import geolocation_router
 from handlers.other_messages import other_messages_router
 
 
 async def main():
     dp.include_router(start_router)
     dp.include_router(menu_router)
+    dp.include_router(geolocation_router)
     dp.include_router(other_messages_router)
 
     await dp.start_polling(bot)
