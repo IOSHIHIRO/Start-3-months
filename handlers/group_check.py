f1c3f6025c57cbad3903140954ea89a2b9c181fc
IOSHIHIRO <emirans23@gmail.com>
2024-11-27 16:46:28 +0600

    dz

--- handlers/group_check.py
+++ handlers/group_check.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+from aiogram.filters import Command
+
 from aiogram import Router,F,types
 
 
@@ -6,22 +8,25 @@ grou_check_router = Router()
 
 BAD_WORDS = ("дурак",)
 
-
+@grou_check_router.message(Command("ban"))
+async def ban(message: types.Message):
+    if not message.reply_to_message:
+        await message.answer("Надо сделать реплэй")
+    else:
+        id = message.reply_to_message.from_user.id
+        await message.bot.ban_chat_member(chat_id=message.chat.id, user_id=id,
+                                          until_date=timedelta(seconds=30,minutes=5,days=1))
 
 @grou_check_router.message(F.text)
 async def group_check(message: types.Message):
     for bad_word in BAD_WORDS:
         if bad_word in message.text.lower():
-            await message.answer("Сам такой")
+            await message.answer("Вы были забанины за плохое поведение ")
+            await message.bot.ban_chat_member(chat_id=message.chat.id,
+                                              user_id=message.from_user.id,
+                                              until_date=timedelta(seconds=10))
             await message.delete()
-            break
-        if message.reply_to_message:
-            user = message.reply_to_message.from_user
-            await message.chat.ban(
-                user_id = user.id,
-                until_date=timedelta(minutes=10)
-            )
-            await message.answer("Вы были забанины")
+
 
 @grou_check_router.message(F.photo)
 async def delete_check(message: types.Message):
